57d737f8d87d991b95eb6979e6b2c0c83da4c959
Heisenberg <divit.singhal2000@gmail.com>
2021-08-02 11:36:34 +0530

    Commit

--- Match.js
+++ Match.js
@@ -116,7 +116,6 @@ function sort_by_key(array, key) {
     return array.sort(function(a, b) {
         var x = a[key];
         var y = b[key];
-        commit
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 }
